772e8ecadc16d013527434de2118a6d82d9b7705
Nicolas DUBIEN <github@dubien.org>
2017-01-09 11:33:05 +0100

    Dangle iterators code on Visual Studio
    According to cppreference, push_back has the following behaviour against
    iterators:
    If the new size() is greater than capacity() then all iterators and
    references (including the past-the-end iterator) are invalidated.
    Otherwise only the past-the-end iterator is invalidated.
    
    Thus std::rend() and std::end() might be invalidated after a push_back.
    It seems to be the case with VS but not with G++ and Clang.

---
+++ main_pushback.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <vector>
+
+#include "gtest/gtest.h" // gtest must be build using /MTd or /MT -DCMAKE_CXX_FLAGS_DEBUG="/MTd"
+
+std::vector<int> pushback_failure(unsigned size)
+{
+  std::vector<int> vs(size);
+  std::iota(std::begin(vs), std::end(vs), 0);
+  
+  vs.reserve(2 * size);
+  std::copy(std::rbegin(vs), std::rend(vs), std::back_inserter(vs));
+  return vs;
+}
+
+TEST(TEST_NAME, SpecificNumberOfValuesFailure)
+{
+  std::vector<int> expected = { 0, 1, 2, 2, 1, 0 };
+  ASSERT_EQ(expected, pushback_failure(3));
+}
+
+TEST(TEST_NAME, SpecificNumberOfValuesOk)
+{
+  std::vector<int> expected = { 0, 1, 2, 2, 1, 0 };
+  ASSERT_EQ(expected, pushback_failure(3));
+}
+
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  int ret { RUN_ALL_TESTS() };
+  return ret;
+}
